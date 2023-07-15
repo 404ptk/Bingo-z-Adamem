d458c76671b0d926936f3dfa21b3e4cfdf6d33ee
utopiasz <108901659+utopiasz@users.noreply.github.com>
2023-07-16 01:07:50 +0200

    v0.02 night mode

--- script.js
+++ script.js
@@ -13,42 +13,48 @@ function togglemenu(){
 
 function startTimer() {
     var startTime;
-    var duration = 3 * 60 * 60 * 1000;
+    var duration = 3 * 60 * 60 * 1000; //3h
 
+    //sprawdzanie czy czas poczatkowy jest w pamieci lokalnej
     if (localStorage.getItem('startTime')) {
-      startTime = parseInt(localStorage.getItem('startTime'));
+        startTime = parseInt(localStorage.getItem('startTime'));
     } else {
-      startTime = Date.now();
-      localStorage.setItem('startTime', startTime.toString());
+        //ustaw poczatkowy na aktualny
+        startTime = Date.now();
+        localStorage.setItem('startTime', startTime.toString());
     }
 
     var timerId = setInterval(function() {
-      var elapsedTime = Date.now() - startTime;
-      var remainingTime = duration - elapsedTime;
+        var elapsedTime = Date.now() - startTime;
+        var remainingTime = duration - elapsedTime;
 
-      if (remainingTime <= 0) {
-        clearInterval(timerId);
-        var timerDisplay = document.getElementById('timerDisplay');
-        timerDisplay.textContent = 'Timer finished!';
-      } else {
-        var hours = Math.floor(remainingTime / (1000 * 60 * 60));
-        var minutes = Math.floor((remainingTime % (1000 * 60 * 60)) / (1000 * 60));
-        var seconds = Math.floor((remainingTime % (1000 * 60)) / 1000);
+        if (remainingTime <= 0) {
+            clearInterval(timerId);
+            var timerDisplay = document.getElementById('timerDisplay');
+            timerDisplay.textContent = 'Timer finished!';
+        } else {
+            var hours = Math.floor(remainingTime / (1000 * 60 * 60));
+            var minutes = Math.floor((remainingTime % (1000 * 60 * 60)) / (1000 * 60));
+            var seconds = Math.floor((remainingTime % (1000 * 60)) / 1000);
 
-        var timerDisplay = document.getElementById('timerDisplay');
-        timerDisplay.textContent =hours + 'h ' + minutes + 'm ' + seconds + 's';
-      }
-    }, 1000); // Odświeżaj co sekundę (1000 milisekund)
+            var timerDisplay = document.getElementById('timerDisplay');
+            timerDisplay.textContent =hours + 'h ' + minutes + 'm ' + seconds + 's';
+        }
+    }, 1000); //odswiezanie co sekunde
 
+    //reset
     var resetButton = document.getElementById('resetButton');
     resetButton.addEventListener('click', function() {
-      startTime = Date.now();
-      localStorage.setItem('startTime', startTime.toString());
+        //resetowanie czasu lokalnego
+        startTime = Date.now();
+        localStorage.setItem('startTime', startTime.toString());
     });
-  }
+}
 
-  window.addEventListener('DOMContentLoaded', startTimer);
+    //start gdy strona jest zaladowana
+    window.addEventListener('DOMContentLoaded', startTimer);
 
+//teksty
 var przykladoweTeksty = [
     'Temat Szyszki',
     'Temat Jaspera',
@@ -85,37 +91,42 @@ var przykladoweTeksty = [
     'Adam został pociśnięty w donejcie',
     'Robienie kontentu na pietrasie',
     'Wpierdalanie na strimie'
-  ];
+];
 
 
+    //zbior tekstow
     var dostepneIndeksy = Array.from(Array(przykladoweTeksty.length).keys());
 
+    //losowanie tekstu
     function losujTekst(element) {
-      if (dostepneIndeksy.length === 0) {
-        element.textContent = 'Brak dostępnych tekstów';
-        return;
-      }
+        if (dostepneIndeksy.length === 0) {
+            element.textContent = 'Brak dostępnych tekstów';
+            return;
+        }
 
-      var losowyIndeks = Math.floor(Math.random() * dostepneIndeksy.length);
-      var wylosowanyIndeks = dostepneIndeksy.splice(losowyIndeks, 1)[0];
-      var wylosowanyTekst = przykladoweTeksty[wylosowanyIndeks];
-      element.textContent = wylosowanyTekst;
+        var losowyIndeks = Math.floor(Math.random() * dostepneIndeksy.length);
+        var wylosowanyIndeks = dostepneIndeksy.splice(losowyIndeks, 1)[0];
+        var wylosowanyTekst = przykladoweTeksty[wylosowanyIndeks];
+        element.textContent = wylosowanyTekst;
     }
 
+    //przywrocenie zapisanego stanu tekstow po otwarciu strony
     window.addEventListener('DOMContentLoaded', function() {
-      var divItems = document.getElementsByClassName('item');
-      Array.from(divItems).forEach(function(item) {
-        losujTekst(item);
-      });
+        var divItems = document.getElementsByClassName('item');
+        Array.from(divItems).forEach(function(item) {
+            losujTekst(item);
+        });
     });
+
+    //reset losowania
     var resetButton = document.getElementById('resetButton');
     resetButton.addEventListener('click', function() {
-      dostepneIndeksy = Array.from(Array(przykladoweTeksty.length).keys());
+        dostepneIndeksy = Array.from(Array(przykladoweTeksty.length).keys());
 
-      var divItems = document.getElementsByClassName('item');
-      Array.from(divItems).forEach(function(item) {
-        losujTekst(item);
-      });
+        var divItems = document.getElementsByClassName('item');
+        Array.from(divItems).forEach(function(item) {
+            losujTekst(item);
+        });
     });
   
 
@@ -129,56 +140,63 @@ var diagonal = 0;
 var antiDiagonal = 0;
 
 function main(element) {
-  var currentColor = element.style.backgroundColor;
-  element.style.backgroundColor = currentColor !== "rgb(166, 245, 189)" ? "rgb(166, 245, 189)" : "rgb(255, 255, 255)";
+    var currentColor = element.style.backgroundColor;
+    element.style.backgroundColor = currentColor !== "rgb(166, 245, 189)" ? "rgb(166, 245, 189)" : "rgb(255, 255, 255)";
 
-  checkWin();
+    checkWin();
 }
 
 function checkWin() {
-  var divItems = document.getElementsByClassName('item');
-  rows = [];
-  columns = [];
-  diagonal = 0;
-  antiDiagonal = 0;
+    var divItems = document.getElementsByClassName('item');
+    rows = [];
+    columns = [];
+    diagonal = 0;
+    antiDiagonal = 0;
 
-  for (var i = 0; i < divItems.length; i++) {
-    var row = Math.floor(i / 4);
-    var column = i % 4;
+    for (var i = 0; i < divItems.length; i++) {
+        var row = Math.floor(i / 4);
+        var column = i % 4;
 
     if (rows[row] === undefined) {
-      rows[row] = true;
+        rows[row] = true;
     }
 
     if (columns[column] === undefined) {
-      columns[column] = true;
+        columns[column] = true;
     }
 
     if (row === column) {
-      diagonal = diagonal || (divItems[i].style.backgroundColor !== "rgb(166, 245, 189)");
+        diagonal = diagonal || (divItems[i].style.backgroundColor !== "rgb(166, 245, 189)");
     }
 
     if (row + column === 3) {
-      antiDiagonal = antiDiagonal || (divItems[i].style.backgroundColor !== "rgb(166, 245, 189)");
+        antiDiagonal = antiDiagonal || (divItems[i].style.backgroundColor !== "rgb(166, 245, 189)");
     }
 
     if (divItems[i].style.backgroundColor !== "rgb(166, 245, 189)") {
-      rows[row] = false;
-      columns[column] = false;
+        rows[row] = false;
+        columns[column] = false;
     }
   }
 
-  var win = rows.includes(true) || columns.includes(true) || !diagonal || !antiDiagonal;
+    var win = rows.includes(true) || columns.includes(true) || !diagonal || !antiDiagonal;
 
-  var resultElement = document.getElementById('result');
-  resultElement.textContent = win ? "Wygrałeś!" : "";
+    var resultElement = document.getElementById('result');
+    resultElement.textContent = win ? "Wygrałeś!" : "";
 }
 
+// Przywróć zapisany stan po otwarciu strony
 window.addEventListener('DOMContentLoaded', function() {
-  var divItems = document.getElementsByClassName('item');
-  for (var i = 0; i < divItems.length; i++) {
-    if (divItems[i].style.backgroundColor === "rgb(166, 245, 189)") {
-      divItems[i].click();
+    var divItems = document.getElementsByClassName('item');
+    for (var i = 0; i < divItems.length; i++) {
+        if (divItems[i].style.backgroundColor === "rgb(166, 245, 189)") {
+        divItems[i].click();
+        }
     }
-  }
 });
+
+
+function darkMode() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+}
